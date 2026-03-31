4151d2307e07a8b50ce55a13af99711b78753806
Tibo De Peuter <tibo@depeuter.dev>
2026-03-31 19:51:17 +0000

    refactor(main): replace print with logging

--- src/main.py
+++ src/main.py
@@ -1,4 +1,9 @@
+import logging
+
 import jax
 
+logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
-    print(jax.devices())
+    logging.basicConfig(level=logging.INFO)
+    logger.info(f"JAX devices: {jax.devices()}")
